6aff1cf043442b4c4448663862a436e921f99de2
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2023-12-17 09:59:26 -0700

    added 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,26 @@ let locationTempEl = document.querySelector("#location-temp");
 let locationWindEl = document.querySelector("#location-wind");
 let locationHumidityEl = document.querySelector("#location-humidity");
 
+let forecastItemsEl = document.querySelector("#forecast-items");
+let dynamicContainerEl = document.querySelector("#dynamic-container");
 let rightPlaceholderEl = document.querySelector("#right-placeholder");
 let rightContainer = document.querySelector("#right-container");
+
+function renderRecentSearches() {
+  recentSearchesEl.innerHTML = "";
+  let recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  if (recentSearches && recentSearches.length > 0) {
+    recentSearches = [...new Set(recentSearches)];
+    recentSearches.forEach((item) => {
+      let recentSearchItem = document.createElement("li");
+      recentSearchItem.id = "recent-search-li";
+      recentSearchItem.textContent = item;
+      recentSearchesEl.append(recentSearchItem);
+    });
+  }
+}
+
+renderRecentSearches();
 
 async function handleSearch(e, shouldPushToLs = true) {
   e.preventDefault();
@@ -48,6 +66,12 @@ async function handleSearch(e, shouldPushToLs = true) {
       }
     }
 
+    let currentDateEl = document.querySelector("#current-date");
+    let weatherIconEl = document.querySelector("#weather-icon");
+    let locationTempEl = document.querySelector("#location-temp");
+    let locationWindEl = document.querySelector("#location-wind");
+    let locationHumidityEl = document.querySelector("#location-humidity");
+
     locationNameEl.textContent = locationData2.city.name;
     currentDateEl.textContent = ` (${new Date().toLocaleDateString()})`;
     weatherIconEl.setAttribute("src", `https://openweathermap.org/img/wn/${usedDateData[0].weather[0].icon}@2x.png`);
@@ -67,8 +91,41 @@ async function handleSearch(e, shouldPushToLs = true) {
       }
     }
 
+    forecastItemsEl.innerHTML = "";
+
+    usedDateData
+      .filter((item) => new Date(item.dt * 1000).getDay() !== new Date().getDay())
+      .forEach((item) => {
+        const forecastItemEl = document.createElement("div");
+        const forecastItemDateEl = document.createElement("p");
+        const forecastItemIconEl = document.createElement("img");
+        const forecastItemTempEl = document.createElement("p");
+        const forecastItemWindEl = document.createElement("p");
+        const forecastItemHumidityEl = document.createElement("p");
+
+        forecastItemEl.id = "forecast-item-container";
+        forecastItemDateEl.id = "forecast-date";
+        forecastItemIconEl.id = "weather-icon";
+        forecastItemTempEl.id = "forecast-etc";
+        forecastItemWindEl.id = "forecast-etc";
+        forecastItemHumidityEl.id = "forecast-etc";
+
+        forecastItemDateEl.innerText = ` (${new Date(item.dt * 1000).toLocaleDateString()})`;
+        forecastItemIconEl.setAttribute("src", `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`);
+        forecastItemTempEl.innerText = `Temp: ${item.main.temp} °F`;
+        forecastItemWindEl.innerText = `Wind: ${item.wind.speed} MPH`;
+        forecastItemHumidityEl.innerText = `Humidity: ${item.main.humidity} %`;
+
+        forecastItemEl.append(forecastItemDateEl, forecastItemIconEl, forecastItemTempEl, forecastItemWindEl, forecastItemHumidityEl);
+        forecastItemsEl.appendChild(forecastItemEl);
+      });
+
     rightPlaceholderEl.classList.add("hidden");
     rightContainer.classList.remove("hidden");
+
+    renderRecentSearches();
+
+    textInput.value = "";
   } catch (e) {
     //error message
     window.alert("no location found. please check spelling");
